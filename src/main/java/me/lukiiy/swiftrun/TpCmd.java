9aae99637bea288337cfd5c011b2aad5b71b3c2f
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2025-12-21 18:00:41 -0300

    add TODO to TpCmd

--- src/main/java/me/lukiiy/swiftrun/TpCmd.java
+++ src/main/java/me/lukiiy/swiftrun/TpCmd.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-public class TpCmd implements BasicCommand {
+public class TpCmd implements BasicCommand { // TODO
     @Override
     public void execute(CommandSourceStack commandSourceStack, String[] strings) {
         CommandSender sender = commandSourceStack.getSender();
